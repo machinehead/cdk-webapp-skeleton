d948a5e5a4ad0aad6ad1cd42830c6c7cb92d95de
Ilya Nekhay <nekhayiv@gmail.com>
2023-03-04 11:10:49 -0600

    0.2.dev4: added basic pipeline

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,9 @@
 from cdk_webapp_skeleton import BranchCICDStack
+from aws_cdk import (
+    pipelines as pipelines,
+)
 
 
 def test_instantiate():
-    BranchCICDStack(None)
+    source = pipelines.CodePipelineSource.connection("owner/repo", "branch", connection_arn="arn")
+    BranchCICDStack(None, source=source)

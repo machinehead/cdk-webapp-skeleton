12c8b2f49f9fd2680a138534c3214f6628734a98
machinehead <nekhayiv@gmail.com>
2025-01-25 12:21:07 -0600

    fix(branch_cicd_pipeline): add ecr:BatchImportUpstreamImage permission for CodeBuild

--- cdk_webapp_skeleton/branch_cicd_pipeline.py
+++ cdk_webapp_skeleton/branch_cicd_pipeline.py
@@ -88,6 +88,7 @@ class BranchCICDPipeline(Construct):
                             "ecr:GetRepositoryPolicy",
                             "ecr:ListImages",
                             "ecr:DescribeImages",
+                            "ecr:BatchImportUpstreamImage",
                         ],
                         resources=["*"],
                     )
